2a291914437124d6a0bc30bc4f15346ff581be3a
Facundo Rodriguez <facundo.rodriguez@mdevz.com>
2022-11-21 13:55:48 -0300

    Agrega lectura y parseo de variables del servidor

--- src/requests.c
+++ src/requests.c
@@ -30,7 +30,53 @@ void perform_requests() {
   		//mc = curl_multi_poll(multi_handle, NULL, 0, 1000, NULL);
 		if(mc)
   		break;
-  } while(still_running);
+  } while(still_running);	
+}
+
+void parse_pseudo_json_response(char* response, t_drop_params* config) {
+   char delim[10] = ",:";
+	char *token = strtok(response, delim);
+   int pos = 0;
+   char param[100];
+
+	while (token != NULL)
+	{     
+      if (pos % 2 == 0) {  // nombre del parametro
+         strcpy(param, token);
+      }
+      if (pos % 2 != 0) { // valor
+         int val = atoi(token);
+         if (strcmp(param, "min_freq") == 0)
+            config->min_freq = val;
+         if (strcmp(param, "max_freq") == 0)
+            config->max_freq = val;
+         if (strcmp(param, "inactivity_time") == 0)
+            config->inactivity_time = val;
+         if (strcmp(param, "output_time") == 0)
+            config->output_time = val;
+         if (strcmp(param, "threshold") == 0)
+            config->threshold = val;
+      }
+		// printf("%d\n", config->min_freq);
+		token = strtok(NULL, delim);
+      pos++;
+	}
+}
+
+static size_t write_callback(void *data, size_t size, size_t nmemb, void *userp) {
+   size_t realsize = size * nmemb;
+   struct memory *mem = (struct memory *)userp;
+
+   void* ptr = realloc(mem->response, mem->size + realsize + 1);
+   if(ptr == NULL)
+      return 0;  /* out of memory! */
+
+   mem->response = (char*)ptr;
+   memcpy(&(mem->response[mem->size]), data, realsize);
+   mem->size += realsize;
+   mem->response[mem->size] = 0;
+
+   return realsize;
 }
 
 void turn_on() {
@@ -45,8 +91,9 @@ void turn_on() {
 	curl_easy_setopt(curl, CURLOPT_POST, 1L);
 	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
 	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
 
-  curl_multi_add_handle(multi_handle, curl);
+   curl_multi_add_handle(multi_handle, curl);
 
 	perform_requests();
 
@@ -66,7 +113,9 @@ void turn_off() {
 	curl_easy_setopt(curl, CURLOPT_POST, 1L);
 	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
 	curl_easy_setopt(curl, CURLOPT_URL, url);
-  curl_multi_add_handle(multi_handle, curl);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+
+   curl_multi_add_handle(multi_handle, curl);
  	
 	perform_requests();
 
@@ -74,23 +123,28 @@ void turn_off() {
 	curl_easy_cleanup(curl);
 }
 
-void get_server_variables() {
+void get_server_variables(t_drop_params* config) {
 	if (!is_server_configured) return;
 
 	char url[100];
 	strcpy(url, base_url);
 	strcat(url, "/variables");
 
+   struct memory chunk = {0};
+
 	CURL* curl;
 	curl = curl_easy_init();
 	curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
 	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
 	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
-  curl_multi_add_handle(multi_handle, curl);
+   curl_multi_add_handle(multi_handle, curl);
 	
 	perform_requests();
-
+	parse_pseudo_json_response(chunk.response, config);
+	
 	curl_multi_remove_handle(multi_handle, curl);
 	curl_easy_cleanup(curl);
 }
--- src/requests.h
+++ src/requests.h
@@ -3,11 +3,19 @@
 
 #include <curl/curl.h>
 #include <string.h>
+#include "config.h"
+
+struct memory {
+   size_t size;
+   char* response;
+};
 
 int init_requests();
 void turn_on();
 void turn_off();
-void get_server_variables();
+void get_server_variables(t_drop_params* config);
 void perform_requests();
+void parse_pseudo_json_response(char* response, t_drop_params* config);
+static size_t write_callback(void *data, size_t size, size_t nmemb, void *userp);
 
 #endif
