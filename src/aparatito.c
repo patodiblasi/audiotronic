617805d9ad516d7c52c21268915f39647b065c63
Facundo Rodriguez <facundo.rodriguez@mdevz.com>
2022-11-21 23:58:28 -0300

    Agrego log cuando leo variables del servidor

--- src/aparatito.c
+++ src/aparatito.c
@@ -4,12 +4,12 @@ int is_on = 0;
 
 int run_aparatito_frame(t_fft* fft, t_drop_params* config) {
    if (is_kick(fft, config)) {
-      if (is_on == 0) {
+      if (!is_on) {
          turn_on();
          is_on = 1;
       }
    } else {
-      if (is_on == 1) {
+      if (is_on) {
          turn_off();
          is_on = 0;
       }
@@ -20,5 +20,6 @@ int run_aparatito_frame(t_fft* fft, t_drop_params* config) {
 
 
 void get_aparatito_variables(t_drop_params* config) {
+   log_info("Leyendo variables del servidor");
    get_server_variables(config);
 }
--- src/aparatito.h
+++ src/aparatito.h
@@ -2,6 +2,7 @@
 #include "requests.h"
 #include "recognition_audio.h"
 #include "process_audio.h"
+#include "log/src/log.h"
 
 int run_aparatito_frame(t_fft* fft, t_drop_params* config);
 
