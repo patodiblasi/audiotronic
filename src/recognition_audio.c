0fe01a1bd538a14f6e9535d27b28db2aafe0effa
Facundo Rodriguez <facundo.rodriguez@mdevz.com>
2022-11-23 10:25:42 -0300

    Agrego logs para ver status

--- src/recognition_audio.c
+++ src/recognition_audio.c
@@ -42,18 +42,28 @@ int is_drop(t_fft* fft, t_drop_params* config)
       elapsed_frames_since_drop = 0;
       was_on = 0;
    }
-
    if (is_kick(fft, config)) {  // Si hay kick
       if(frames_without_kick / frames_to_time >= config->inactivity_time) {  // Y el tiempo sin kick que paso es mayor a x 
          frames_without_kick = 0;  // Reinicio el conteo de tiempo sin kick
          was_on = 1;
+         log_info("DROP!");
          return 1;  // Debo enviar señal al servidr.
       }
       frames_without_kick = 0;  // Reinicio el conteo de tiempo sin kick
       return 0;
    }
-   
    // Si no hubo kick
+   char msg[100]; // Logeo msj para saber donde estamos parados
+   int countdown = config->inactivity_time - frames_without_kick / frames_to_time;
+   if(countdown == config->inactivity_time) {
+      sprintf(msg, "Kick mode...Esperando bajada");
+   } else if (countdown > 0) {
+      sprintf(msg, "Entrando en bajada mode en %is", countdown);
+   } else {
+      sprintf(msg, "Bajada mode...Esperando drop entre %i-%i Hz, mayor a %.f%%", config->min_freq, config->max_freq, config->threshold / 1024.0f * 100);
+   }
+   log_info(msg);
+   
    frames_without_kick++;  // Incrementar tiempo sin kick
    return 0;  // Seguro que la salida deberia estar apagada
 }
